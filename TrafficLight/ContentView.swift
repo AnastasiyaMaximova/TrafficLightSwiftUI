8002ff4d15ee874cb41395d9052161180d4deded
AnastasyiaMaximova <rsk.maximova@bk.ru>
2025-01-19 12:00:33 +0300

    initial commit

--- TrafficLight/ContentView.swift
+++ TrafficLight/ContentView.swift
@@ -10,10 +10,30 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Circle()
+                .fill(.red)
+                .frame(width: 200)
+                .overlay(Circle().stroke(Color.black, lineWidth: 6))
+                .shadow(radius: 10)
+            Circle()
+                .fill(.yellow)
+                .frame(width: 200)
+                .overlay(Circle().stroke(Color.black, lineWidth: 6))
+                .shadow(radius: 10)
+            Circle()
+                .fill(.green)
+                .frame(width: 200)
+                .overlay(Circle().stroke(Color.black, lineWidth: 6))
+                .shadow(radius: 10)
+            Button(action: {}){Text("Start")
+                    .font(.largeTitle)
+            }
+            .frame(width: 200, height: 60)
+           // .cornerRadius(200)
+            .overlay(Capsule().stroke(Color.black, lineWidth: 6))
+            .background(.tint)
+            .foregroundStyle(.white)
+            .padding(.top, 40)
         }
         .padding()
     }
