9fc1d0311c7dead8543d5c409f8ae8368cce388f
AnastasyiaMaximova <rsk.maximova@bk.ru>
2025-01-19 11:09:48 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Anastasya Maximova on 19.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrafficLightTests/TrafficLightTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightTests.swift
+//  TrafficLightTests
+//
+//  Created by Anastasya Maximova on 19.01.2025.
+//
+
+import Testing
+@testable import TrafficLight
+
+struct TrafficLightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
